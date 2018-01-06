039d0d1e79fd1d2fd5ae77a06b95883a5135861f
VGamezz19 <victor.gamez1596@gmail.com>
2018-01-06 04:35:50 +0100

    class 3

---
+++ jacaScript.js
@@ -0,0 +1,50 @@
+//Class 1
+
+//Comilla invertida ``
+console.log(`==================== Class 1 ====================`)
+
+let base = 5
+let height = 7
+const  triangleArea = (base, height) => base * height / 2
+console.log(`Area de un triangulo de base ${base} y altura ${height} = ${triangleArea(base,height)}`)
+
+console.log(`==================== Class 2 ====================`)
+
+const   starWars7 = 'StarWars Peli Reshulona',
+        pgStarWars7 = 13,
+        nameVgamez ='Victor',
+        ageVgamez = 21,
+        nameSanti = 'Santi',
+        ageSanti = 12,
+        canWatchStarWars7 = (name, age) => (age) >= pgStarWars7 ? alert(`${name}, puede pasar a ver la peli`) : alert(`${name}, no puede ver la peli`)
+        canWatchStarWars7(nameVgamez,ageVgamez)
+        canWatchStarWars7(nameSanti,ageSanti)
+
+console.log(`==================== Class 3 ====================`)
+// 1- Si la palabra termina en "ar", se le quitan esos los dos caracteres
+// 2- Si la palabra inicia con Z, se le añade "pe" al final
+// 3- Si tiene 10 o mas caracteres, hay que ponerle un guion en la mitad
+// 4- Si es un palindromo, ninguna regla anterior cuenta y se devuelve intercalando mayusculas y minusculas
+const platzom = (str) => {
+    let translation = str
+    //1
+    translation.toLowerCase().endsWith('ar') ? translation = str.slice(0,-2) : translation
+    //2
+    translation.toLowerCase().startsWith('z') ? translation += 'pe' : translation
+    //3
+    const length = translation.length,
+          half = Math.round(translation.length / 2);
+    length >=  10 ? translation = `${translation.slice(0,half)} - ${translation.slice(half)}`: translation
+    //4
+    const reverse = str => str.split('').reverse().join(''),
+          minMay  = str => str.split('').map(c => str.indexOf(c) % 2 == 0 ? c.toUpperCase() : c.toLowerCase()).join('')
+    
+    
+    translation =  str === reverse(translation) ? minMay(str) : translation
+
+    return translation
+}
+
+console.log(platzom('zarpar'))
+console.log(platzom('abecedario'))
+console.log(platzom('sometemos'))
