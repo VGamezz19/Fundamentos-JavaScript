de507cd18cabc048df083303da62c5929a27246d
VGamezz19 <victor.gamez1596@gmail.com>
2018-01-07 05:03:52 +0100

    Class 18

--- jacaScript.js
+++ jacaScript.js
@@ -355,5 +355,29 @@ class Persona {
     }/* .bind(this) */)
   }
 }
-
 const sacha = new Persona("Sacha", [ "Pedro", "Juan", "Pepe" ])
+
+console.log(`==================== Class 18 ====================`)
+class Toggable {
+  constructor(el) {
+    // inicializar el estado interno
+    this.el = el
+    this.el.innerHTML = 'Off'
+    this.activated = false
+    this.onClick = this.onClick.bind(this)
+    this.el.addEventListener('click', this.onClick)
+  }
+
+  onClick(ev) {
+    this.activated = !this.activated
+    this.toggleText()
+  }
+
+  toggleText() {
+    this.el.innerHTML = this.activated ? 'On' : 'Off'
+  }
+}
+
+const button = document.getElementById('boton')
+
+const miBoton = new Toggable(button)
