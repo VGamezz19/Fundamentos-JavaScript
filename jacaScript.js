471b69e9f1cbcfc5ba6eda49c081639d8d64e466
VGamezz19 <victor.gamez1596@gmail.com>
2018-01-08 17:48:28 +0100

    Class 19

--- jacaScript.js
+++ jacaScript.js
@@ -381,3 +381,20 @@ class Toggable {
 const button = document.getElementById('boton')
 
 const miBoton = new Toggable(button)
+
+console.log(`==================== Class 19 ====================`)
+const sacha = {
+  nombre: 'Sacha',
+  apellido: 'Lifszyc'
+}
+
+function saludar(veces, uppercase) {
+  let str = `Hola ${this.nombre} ${this.apellido}`
+  str = uppercase ? str.toUpperCase() : str
+  for (let i = 0; i < veces; i++) {
+    console.log(str)
+  }
+}
+
+const params = [3, true]
+saludar.call(sacha, ...params)
