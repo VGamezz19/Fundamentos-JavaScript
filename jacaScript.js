9570f6acde09568b73e3f9edaef9aaf11704921f
VGamezz19 <victor.gamez1596@gmail.com>
2018-01-10 01:04:52 +0100

    Class 36

--- jacaScript.js
+++ jacaScript.js
@@ -574,3 +574,24 @@ for (let value of fibo) {
 	i++
 	if(i>20) break
 }
+
+console.log(`==================== Class 36 ====================`)
+function* fibonacci(){
+	let a=0, b=1
+	//yield a - hace que la siguiente vez que se ejecute 
+	// la función continue en la siguiente linea.
+	
+	while (true) {
+		let f= a
+		a = b
+		b = f + a
+    let reset = yield f
+    console.log(reset)
+		if(reset) a=0, b=1
+	}
+}
+const fibo = fibonacci()
+// Comandos para la ejecución en la consola
+fibo 
+fibo.next()
+fibo.next(reset)
